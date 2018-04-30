240c6eddf37bdf2456a021919b2fe38742223a9f
lex-attax <lex.pavlovic@gmail.com>
2018-04-30 14:09:52 +0200

    Add files via upload

---
+++ mindstorms.py
@@ -0,0 +1,23 @@
+import turtle
+
+def draw_square(some_turtle):
+    some_turtle.forward(100)
+    some_turtle.right(90)
+
+def draw_art():
+    window = turtle.Screen()
+    window.bgcolor("red")
+    #create the turtle brad - draws a square
+    bard = turtle.Turtle()
+    brad.shape("turtle")
+    brad.color("yellow")
+    brad.speed(2)
+    draw.square(brad)
+    #create the turtle angie - draws a circle
+    angie = turtle.Turlte()
+    angie.shape("arrow")
+    angie.color("blue")
+    angie.circle(100)
+
+    windows.exitonclick()
+draw_art()
