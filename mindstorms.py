aaf0eaf649150f12e86a8c59d6651f7335e2605d
lex-attax <lex.pavlovic@gmail.com>
2018-04-30 14:12:39 +0200

    fix typos

--- mindstorms.py
+++ mindstorms.py
@@ -1,20 +1,21 @@
 import turtle
 
 def draw_square(some_turtle):
-    some_turtle.forward(100)
-    some_turtle.right(90)
+    for i in range(1,5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
 
 def draw_art():
     window = turtle.Screen()
     window.bgcolor("red")
     #create the turtle brad - draws a square
-    bard = turtle.Turtle()
+    brad = turtle.Turtle()
     brad.shape("turtle")
     brad.color("yellow")
     brad.speed(2)
-    draw.square(brad)
+    draw_square(brad)
     #create the turtle angie - draws a circle
-    angie = turtle.Turlte()
+    angie = turtle.Turtle()
     angie.shape("arrow")
     angie.color("blue")
     angie.circle(100)
